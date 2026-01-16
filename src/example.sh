eb54afd72d52e6ba9962dcdaeef5f1febdc72560
Christopher Handy <handyc@helxt.com>
2026-01-16 20:49:25 +0100

    Update example.sh
    simpler

--- src/example.sh
+++ src/example.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-af=4; ab=2; tput setaf $af; tput setab $ab; clear;
+af=3; ab=5; tput setaf $af; tput setab $ab; clear;
 CHARS=( a b c d e f g h i )
 
 recto () {
+WIDTH=$(( ( RANDOM % 50 )  + 1 ))
+BEG=$(($RANDOM% $WIDTH)); randomi2=$(($RANDOM%24))
+titl=1; titlb=11;
+ysize=$(($RANDOM%12))
 sequence_a=""
-for z in $(seq 0 $(($RANDOM%40))); do
+
+for z in $(seq 0 $WIDTH); do
 SELECTED_CHAR=${CHARS[$RANDOM % ${#CHARS[@]}]};
 sequence_a="$sequence_a$SELECTED_CHAR"; done
 
@@ -16,8 +21,6 @@ for goo in $(seq 1 $(($ysize))); do
 tput cup $(($randomi2+$goo)) $BEG; printf "$sequence_a"; done
 }
 
-BELEND=20; for i in $(seq 0 $BELEND); do
-BEG=$(($RANDOM%70)); randomi2=$(($RANDOM%24))
-titl=$(($RANDOM%16)); titlb=$(($RANDOM%16)); ysize=$(($RANDOM%12))
+BELEND=12; for i in $(seq 0 $BELEND); do
 recto; sleep 1; done
 tput sgr0; tput cup 0 0;
