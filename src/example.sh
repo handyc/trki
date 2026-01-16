4e272d6ec70fc7d7bded9be881cc7b23bfc48fb6
Christopher Handy <handyc@helxt.com>
2026-01-16 19:11:33 +0100

    Update example.sh
    elimination of a small bug (I hope)

--- src/example.sh
+++ src/example.sh
@@ -2,27 +2,29 @@
 CHARS=( a b c d e f g h i )
 
 recto () {
-for z in $(seq 0 5); do
+sequence_a=""
+for z in $(seq 0 $(($RANDOM%40))); do
 SELECTED_CHAR=${CHARS[$RANDOM % ${#CHARS[@]}]};
 sequence_a="$sequence_a$SELECTED_CHAR"
 done
 
 af=$titl; ab=$titlb; tput setaf $af; tput setab $ab;
-tput cup $randomi2 $i; printf "$sequence_a"
+tput cup $randomi2 $BEG; printf "$sequence_a"
 
 af=2; ab=4; tput setaf $af; tput setab $ab;
 for goo in $(seq 1 $(($ysize))); do
-tput cup $(($randomi2+$goo)) $i;
+tput cup $(($randomi2+$goo)) $BEG;
 printf "$sequence_a"
 done
 }
 
-BELEND=10
+BELEND=20
 for i in $(seq 0 $BELEND); do
 BEG=$(($RANDOM%70))
-END=$(($BEG+$(($RANDOM%10))+1))
+END=$(($BEG+$(($RANDOM%70))+1))
+randomi=$(($RANDOM%79))
 randomi2=$(($RANDOM%24))
 titl=$(($RANDOM%16)); titlb=$(($RANDOM%16))
-ysize=$(($RANDOM%10))
+ysize=$(($RANDOM%12))
 recto; sleep 1; done
 tput sgr0;
